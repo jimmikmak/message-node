e8492a639f82580e6391944c786c0f7ce4ad8ccd
James McCarron <mccarron.j@gmail.com>
2022-08-19 15:15:12 +1000

    Invalid React child error persists

--- server/controllers/feed.js
+++ server/controllers/feed.js
@@ -1,3 +1,5 @@
+const { validationResult } = require("express-validator");
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
@@ -16,6 +18,12 @@ exports.getPosts = (req, res, next) => {
 };
 
 exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error("Validation failed, entered data is incorrect.");
+    error.statusCode = 422;
+    throw error;
+  }
   const title = req.body.title;
   const content = req.body.content;
   // Create post in db
