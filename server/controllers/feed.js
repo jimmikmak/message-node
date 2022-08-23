1dfcd36acdaeae97772f5516bc7ca4b6308fc03b
James McCarron <mccarron.j@gmail.com>
2022-08-23 13:03:58 +1000

    Added custom error handling in app.js

--- server/controllers/feed.js
+++ server/controllers/feed.js
@@ -44,6 +44,9 @@ exports.createPost = (req, res, next) => {
       });
     })
     .catch((err) => {
-      console.log(err);
+      if (err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
     });
 };
